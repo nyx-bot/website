c54021a64ba8f7a5aa0f23544017bbfa8428c6c9
Syl <hi@sylviiu.dev>
2022-12-30 02:22:14 -0600

    update assets endpoint to just Work

--- endpoints/assets.js
+++ endpoints/assets.js
@@ -1,8 +1,15 @@
+const fs = require('fs')
+
 module.exports = {
     endpoint: `/assets:path(*+)`,
     func: (req, res) => {
         if(!req.params.path.startsWith(`/`)) req.params.path = `/` + req.params.path
-        const path = __dirname + req.params.path;
-        console.log(path)
+        const path = __dirname.replace(`/endpoints`, ``) + `/html/assets` + req.params.path;
+
+        if(fs.existsSync(path)) {
+            res.sendFile(path)
+        } else {
+            res.sendFile(__dirname.replace(`/endpoints`, ``) + `/html/assets/img/null.png`)
+        }
     }
 }
