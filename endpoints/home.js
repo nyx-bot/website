b9a2ae14a74a9e94fc40463b07107f1b4c78182b
Syl <hi@sylviiu.dev>
2022-12-30 02:22:32 -0600

    add homepage

--- endpoints/home.js
+++ endpoints/home.js
@@ -1,6 +1,7 @@
 module.exports = {
     endpoint: `/`,
     func: async (req, res) => {
-        
+        const home = require('fs').readFileSync(`./html/home.html`).toString();
+        res.send(home)
     }
 }
